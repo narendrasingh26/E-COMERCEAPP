4536d7d20e82335989b631500416da67ffb1cce2
narendrasingh26 <narendrajadon261990@gmail.com>
2023-05-15 13:50:29 +0530

    task2

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -1,37 +1,26 @@
 import React from "react";
-
-import { Row, Card, Col } from "react-bootstrap";
+import { Card, Row, Col, Button, Container } from "react-bootstrap";
+import "./ProductList.css";
 
 let Products = [
   {
     title: "Colors",
-
     price: 100,
-
     imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
   },
-
   {
     title: "Black and white Colors",
-
     price: 50,
-
     imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
   },
-
   {
     title: "Yellow and Black Colors",
-
     price: 70,
-
     imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
   },
-
   {
     title: "Blue Color",
-
     price: 100,
-
     imageUrl: "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
   },
 ];
@@ -39,19 +28,200 @@ let Products = [
 const ProductList = () => {
   return (
     <div>
-      <Row>
-        {Products.map((product, index) => (
-          <Col key={index} md={3}>
-            <Card>
-              <Card.Img variant="top" src={product.imageUrl} alt="productimg" />
-              <Card.Body>
-                <Card.Title>{product.title}</Card.Title>
-                <Card.Text>${product.price}</Card.Text>
-              </Card.Body>
-            </Card>
+      <div
+        style={{
+          textAlign: "center",
+          fontFamily: "serif",
+          backgroundColor: "gray",
+        }}
+      >
+        <h1
+          style={{
+            height: "8rem",
+            fontSize: "5rem",
+            color: "white",
+            fontWeight: "bold",
+          }}
+        >
+          The Generics
+        </h1>
+      </div>
+      <Container className="container-xxl">
+        <p
+          style={{
+            textAlign: "center",
+            fontFamily: "monospace",
+            fontWeight: "bold",
+            fontSize: "2rem",
+          }}
+        >
+          Music
+        </p>
+        <Row className="gy-5" style={{ marginLeft: "20%" }}>
+          <Col md={1} lg={7}>
+            <Row>
+              <Col md={6} lg={6}>
+                <Card
+                  style={{
+                    height: "",
+                    width: "100%",
+                    marginRight: "8000rem",
+                  }}
+                >
+                  <h2
+                    style={{
+                      textAlign: "center",
+                      fontSize: "122%",
+                      fontFamily: "serif",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    Album 1
+                  </h2>
+                  <div className="image-zoom">
+                    <Card.Img
+                      variant="top"
+                      src={Products[0].imageUrl}
+                      alt="productimg"
+                    />
+                  </div>
+                  <Card.Body>
+                    {/* <Card.Title>{Products[0].title}</Card.Title> */}
+                    <Card.Text style={{ fontWeight: "bold" }}>
+                      ${Products[0].price}
+                    </Card.Text>
+                    <Button
+                      className="btn btn-info"
+                      style={{
+                        marginLeft: "20%",
+                        color: "white",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      ADD TO CART
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col md={6} lg={6}>
+                <Card style={{ width: "100%", marginLeft: "46%" }}>
+                  <h2
+                    style={{
+                      textAlign: "center",
+                      fontSize: "122%",
+                      fontFamily: "serif",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    Album 2
+                  </h2>
+                  <div className="image-zoom">
+                    <Card.Img
+                      variant="top"
+                      src={Products[1].imageUrl}
+                      alt="productimg"
+                    />
+                  </div>
+                  <Card.Body>
+                    {/* <Card.Title>{Products[1].title}</Card.Title> */}
+                    <Card.Text style={{ fontWeight: "bold" }}>
+                      ${Products[1].price}
+                    </Card.Text>
+                    <Button
+                      className="btn btn-info"
+                      style={{
+                        marginLeft: "20%",
+                        color: "white",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      ADD TO CART
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
           </Col>
-        ))}
-      </Row>
+          <Col md={6} lg={6}>
+            <Row>
+              <Col md={10} lg={6}>
+                <Card style={{ width: "120%" }}>
+                  <h2
+                    style={{
+                      textAlign: "center",
+                      fontSize: "122%",
+                      fontFamily: "serif",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    Album 3
+                  </h2>
+                  <div className="image-zoom">
+                    <Card.Img
+                      variant="top"
+                      src={Products[2].imageUrl}
+                      alt="productimg"
+                    />
+                  </div>
+                  <Card.Body>
+                    {/* <Card.Title>{Products[2].title}</Card.Title> */}
+                    <Card.Text style={{ fontWeight: "bold" }}>
+                      ${Products[2].price}
+                    </Card.Text>
+                    <Button
+                      className="btn btn-info"
+                      style={{
+                        marginLeft: "20%",
+                        fontWeight: "bold",
+                        color: "white",
+                      }}
+                    >
+                      ADD TO CART
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col md={6} lg={6}>
+                <Card style={{ marginLeft: "72%", width: "120%" }}>
+                  <h2
+                    style={{
+                      textAlign: "center",
+                      fontSize: "122%",
+                      fontFamily: "serif",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    Album 4
+                  </h2>
+                  <div className="image-zoom">
+                    <Card.Img
+                      variant="top"
+                      src={Products[3].imageUrl}
+                      alt="productimg"
+                    />
+                  </div>
+                  <Card.Body>
+                    {/* <Card.Title>{Products[3].title}</Card.Title> */}
+                    <Card.Text style={{ fontWeight: "bold" }}>
+                      ${Products[3].price}
+                    </Card.Text>
+                    <Button
+                      className="btn btn-info"
+                      style={{
+                        marginLeft: "20%",
+                        color: "white",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      ADD TO CART
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
